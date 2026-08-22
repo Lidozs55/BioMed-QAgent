4c4cacf9f29d51d5b4f506602fcfd6eb5d7a81eb
modenicheng <cheng20070225@outlook.com>
2026-08-22 15:20:49 +0800

    feat(dataset): add FamilySpec topology linter

---
+++ server/src/dataset/family-spec-topology/index.ts
@@ -0,0 +1,11 @@
+export {
+  checkFamilySpecTopology,
+  type FamilySpecTopologyIssue,
+  type FamilySpecTopologyIssueCode,
+  type FamilySpecTopologyReport,
+  type NormalizedDeclaredOutputTopology,
+  type NormalizedFamilySpecTopology,
+  type NormalizedProjectionTopology,
+  type NormalizedRelationTopology,
+  type NormalizedTableTopology,
+} from "./topology.js";
